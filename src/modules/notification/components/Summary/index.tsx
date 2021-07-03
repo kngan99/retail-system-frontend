18aa8657e9c552507631f45d864a909295663e60
tustran9499 <minhtu.tran.09041999@gmail.com>
2021-07-03 22:33:57 +0700

    [WIP] - Show notification

--- src/modules/notification/components/Summary/index.tsx
+++ src/modules/notification/components/Summary/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { observer } from 'mobx-react-lite';
+import { List, message, Avatar, Spin } from 'antd';
 import { Button, ListGroup, Badge } from 'react-bootstrap';
+import InfiniteScroll from 'react-infinite-scroller';
+import http from "../../../../common/sevices";
 
 /*
  * Props of Component
@@ -21,8 +24,27 @@ const NotificationSummary = (props: ComponentProps) => {
   const [showNotification, setShowNotification] = React.useState<boolean>(
     false
   );
+  const [data, setData] = React.useState<any[]>([]);
+  const [loading, setLoading] = React.useState<boolean>(false);
+  const [hasMore, setHasMore] = React.useState<boolean>(true);
+  const domain = "http://localhost:4000";
+  const [url, setUrl] = React.useState<string>(domain + "/api/notifications/pagination?page=1&limit=1");
 
-  //const notiStore = React.useContext(NotificationStoreContext);
+  const handleInfiniteOnLoad = () => {
+    setLoading(true);
+    http.get(url).then((res) => {
+      let pastData = data;
+      setData(pastData.concat(res.data.items));
+      if (res.data.links.next && res.data.links.next !== "") {
+        setHasMore(true);
+        setUrl(res.data.links.next);
+      }
+      else {
+        setHasMore(false);
+      }
+      setLoading(false);
+    });
+  };
 
   return (
     <>
@@ -42,7 +64,32 @@ const NotificationSummary = (props: ComponentProps) => {
                   as="li"
                 >
                 </ListGroup.Item>
-              ))
+              <InfiniteScroll
+                initialLoad={true}
+                pageStart={0}
+                loadMore={handleInfiniteOnLoad}
+                hasMore={!loading && hasMore}
+                useWindow={false}
+              >
+                <List
+                  dataSource={data}
+                  renderItem={item => (
+                    <List.Item key={item.id}>
+                      <List.Item.Meta
+                        title={<a href="https://ant.design">{item.Title}</a>}
+                        description={item.Message}
+                      />
+                      <div>{item.CreatedAt}</div>
+                    </List.Item>
+                  )}
+                >
+                  {loading && hasMore && (
+                    <div className="demo-loading-container">
+                      <Spin />
+                    </div>
+                  )}
+                </List>
+              </InfiniteScroll>
                 <ListGroup.Item className="buttons">
                   <Button
                   >
@@ -52,6 +99,9 @@ const NotificationSummary = (props: ComponentProps) => {
                 </ListGroup.Item>
               )
             </ListGroup>
+            {/* <div className="demo-infinite-container"> */}
+            {/* </div> */}
+            {/* <LoadMoreListNotification/> */}
           </>
         )}
       </div>
